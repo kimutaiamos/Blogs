aa756b5c2a59bb0a0501ba9d593756dac97b5478
kimutaiamos <amos.kimutai@student.moringaschool.com>
2021-11-12 23:37:49 +0300

    added the login authenthication

--- app/auth/views.py
+++ app/auth/views.py
@@ -24,7 +24,30 @@ def register():
     title = "registration"
 
     return render_template('auth/register.html', RegistrationForm = form, title = title)
+
+
+
+#logging in function
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    """
+    function that checks if the form is validated
+    """
+
+    login_form = loginForm()
+    if login_form.validate_on_submit():
+        user = User.query.filter_by(email = login_form.email.data).first()
+        if user is not None and user.verify_password(login_form.password.data):
+            login_user(user, login_form.remember.data)
+            return redirect(request.args.get('next')or url_for('main.index'))
+
+
+            flash('Invalid Username or password')
+
+    title = "blogs"
+    return render_template('auth/login.html',login_form = login_form, title = title)
     
+
 
 
 
