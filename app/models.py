71e04d23cb3a3c825e9b009a064c8c06215eb891
kimutaiamos <amos.kimutai@student.moringaschool.com>
2021-11-13 12:47:00 +0300

    addded class methods to define users posts

--- app/models.py
+++ app/models.py
@@ -67,4 +67,49 @@ class User(UserMixin, db.Model):
     #string to print out a row of column important in debugging
     def __repr__(self):
         return f"User {self.username}"
+
+
+
+class Post(db.model):
+    __table__ = "posts"
+
+
+    id = db.Column(db.Integer, primary_key = True)
+    post_title = db.Column(db.String)
+    post_content = db.Column(db.Text)
+    posted_at = db.Column(db.DateTime)
+    upvotes = db.Column(db.Integer, default = 0)
+    downvotes = db.Column(db.Integer, default = 0)
+    post_by = db.Column(db.String)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    comments = db.relationship("Comment", 
+                                    foreign_keys = "comment.post_id",
+                                    backref = "post",
+                                    lazy = "dynamic")
+    
+
+    def save_post(self):
+        db.session.add(self)
+        db.session.commit()
+    
+    def delete_post(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    @classmethod
+    def get_user_posts(cls,id):
+        posts = Post.query.filter_by(user_id  = id).order_by(Post.posted_at.desc()).all()
+        return posts
+
+    @classmethod
+    def get_all_posts(cls):
+        return Post.query.order_by(Post.posted_at).all()
+    @classmethod
+    def get_all_posts(cls):
+        return Post.query.order_by(Post.posted_at).all()
+
+    
+
+      
+
        
