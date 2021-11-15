e594901c04f51340a9591295109de0f3aac38237
kimutaiamos <amos.kimutai@student.moringaschool.com>
2021-11-15 15:00:45 +0300

    added migrations

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 class Config:
 
     
-    exportSQLALCHEMY_DATABASE_URI='postgresql+psycopg2://{moringa-school}:{12345}@localhost/{blogs}'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://qpbzntypxoxtru:d697aeba98bca2a973554ab25e8617bb45086902792925fa1add4d9eedd0688e@ec2-23-22-191-232.compute-1.amazonaws.com:5432/d1v1hl204cn7cu'
 
     # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa-school:12345@localhost/blogs'
 
@@ -15,9 +15,9 @@ class Config:
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
-    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
-    SECRET_KEY= os.environ.get('54a12f8986b077954dcf2ad7f14d07ae')
+    MAIL_USERNAME = ('kimutaiamos82@gmail.com')
+    MAIL_PASSWORD = ('12345')
+    SECRET_KEY= ('54a12f8986b077954dcf2ad7f14d07ae')
     SENDER_EMAIL = 'kimutaiamos82@gmail.com'
   
    #simplemde configurations
@@ -34,7 +34,7 @@ class ProdConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = ('postgresql://qpbzntypxoxtru:d697aeba98bca2a973554ab25e8617bb45086902792925fa1add4d9eedd0688e@ec2-23-22-191-232.compute-1.amazonaws.com:5432/d1v1hl204cn7cu')
 
 
 
@@ -44,7 +44,7 @@ class DevConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-    SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://moringa-school:12345@localhost/blogs'
+    SQLALCHEMY_DATABASE_URI ='postgresql://qpbzntypxoxtru:d697aeba98bca2a973554ab25e8617bb45086902792925fa1add4d9eedd0688e@ec2-23-22-191-232.compute-1.amazonaws.com:5432/d1v1hl204cn7cu'
     # app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:12345@localhost/blogs"
 
     DEBUG = True
